2482d99e60325e2ba56438a496ad3d1050284f22
Cisco Ramon <aungkoman@gmail.com>
2025-05-17 21:22:41 +0630

    exam module is added to admin panel

---
+++ app/Filament/Resources/ExamResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ExamResource\Pages;
+use App\Filament\Resources\ExamResource\RelationManagers;
+use App\Models\Exam;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ExamResource extends Resource
+{
+    protected static ?string $model = Exam::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+
+                DatePicker::make('date')
+                    ->required(),
+
+                Select::make('subject_id')
+                    ->relationship('subject', 'name') // assumes Subject model has a 'name' column
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('serial_no')->label('No.')->rowIndex(),
+                TextColumn::make('name')->searchable()->sortable(),
+                TextColumn::make('date')->dateTime()->sortable(),
+                TextColumn::make('subject.name')->label('Subject')->sortable()->searchable(),
+                TextColumn::make('created_at')->dateTime()->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListExams::route('/'),
+            'create' => Pages\CreateExam::route('/create'),
+            'edit' => Pages\EditExam::route('/{record}/edit'),
+        ];
+    }
+}
